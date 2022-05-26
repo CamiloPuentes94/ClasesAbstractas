94df9bf70b5d9f23b3420599a169b426ebf98f44
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-05-26 11:42:27 -0500

    Se realiza  codigo de main

--- ClasesAbstracta/Program.cs
+++ ClasesAbstracta/Program.cs
@@ -6,7 +6,31 @@ namespace ClasesAbstracta
     {
         static void Main(string[] args)
         {
-            
+            Caballo miCaballo = new Caballo("Babieca");
+            ISaltoConPatas ImiBabieca = miCaballo;
+            Humano Camilo = new Humano();
+            Gorila copito = new Gorila("Copito");
+
+            Mamiferos[] almacenAnimales = new Mamiferos[3];
+
+            almacenAnimales[0] = miCaballo;
+            almacenAnimales[1] = Camilo;
+            almacenAnimales[2] = copito;
+
+            for (int i = 0; i < 3; i++)
+            {
+                almacenAnimales[i].pensar();
+            }
+
+            Mamiferos miMamifero = new Mamiferos("");
+
+            miMamifero.respirar();
+
+            Ballena wally = new Ballena("Wally");
+            wally.nadar();
+
+            Console.WriteLine("Patas utilizadas en el saltode babieca: " + ImiBabieca.numeroPatas());
+
         }
     }
 }
