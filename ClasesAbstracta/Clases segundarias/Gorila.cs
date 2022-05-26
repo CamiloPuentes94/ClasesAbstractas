f2cba4b9fcb73b7edf4933808e5198021c6ce507
Juan Camilo Puentes PC <jcpsandoval94@gmail.com>
2022-05-26 13:55:31 -0500

    Se agrega programacion de clase chimpanse

--- ClasesAbstracta/Clases segundarias/Gorila.cs
+++ ClasesAbstracta/Clases segundarias/Gorila.cs
@@ -1,7 +1,24 @@
-﻿namespace ClasesAbstracta
+﻿using System;
+
+namespace ClasesAbstracta
 {
-    public class Gorila
+    public class Gorila: Mamiferos, IMamiferisTerrestres
     {
-        
+        public Gorila(string nombre) : base(nombre)
+        {
+            
+        }
+
+        public override void Pensar()
+        {
+            Console.WriteLine("Pensamiento instintivo avanzado");
+        }
+
+        public void Trepar()
+        {
+            Console.WriteLine("Soy capaz de trepar");
+        }
+
+        public int NumeroPatas() => 2;
     }
 }
